3a4747dbf198320b736b55f0743e5340c8b26a03
camilac07 <camilacondeisla007@gmail.com>
2024-06-02 23:53:13 -0500

    feat(booking): added booking components

--- src/app/booking/components/booking-create/booking-create.component.ts
+++ src/app/booking/components/booking-create/booking-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-booking-create',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './booking-create.component.css'
 })
 export class BookingCreateComponent {
+  bookingForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.bookingForm = this.fb.group({
+      fechaReserva: [''],
+      nombreDueno: [''],
+      motivoCita: [''],
+      tipoServicio: [''],
+      telefono: [''],
+      detallesAdicionales: ['']
+    });
+  }
+
+  onSubmit() {
+    console.log(this.bookingForm.value);
+  }
 }
